edf42e613f2eacc161ab12ebefbcbc15dbf95a01
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2026-04-20 10:48:22 -0400

    Created navBar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './components/home-page/home-page';
+import { Navbar } from './components/navbar/navbar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', component: HomePage},
+    
+    { path: '**', redirectTo: '' ,pathMatch: 'full'} 
+];
