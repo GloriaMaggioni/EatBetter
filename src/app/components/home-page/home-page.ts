250c68306987ca96597b55135f1b88f2700597c9
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2026-04-27 22:02:30 -0400

    Creato sezione hero

--- src/app/components/home-page/home-page.ts
+++ src/app/components/home-page/home-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../navbar/navbar";
+import { HeroSection } from "../hero-section/hero-section";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [HeroSection],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
