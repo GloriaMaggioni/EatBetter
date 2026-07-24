fa930121ad60c298272ab6522480a81f955342ce
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2026-07-24 10:14:22 -0400

    Aggiunto netlify

---
+++ netlify-proxy/netlify/functions/fruit-proxy.js
@@ -0,0 +1,29 @@
+export default async (req) => {
+    const url = new URL(req.url);
+    const target = url.searchParams.get('url');
+
+
+    if(!target){
+        return new Response(JSON.stringify({error : 'Missing url parameter'}), {
+            status: 400,
+            headers : {'Content-Type': 'application/json'}
+        })
+    }
+
+
+    const response = await fetch(target);
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+    });
+
+
+    export const config = {
+        path: "/api/fruit-proxy"
+    }
+}
